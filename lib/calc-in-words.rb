d0f488d549c462b21d93af2d433d85cba5573d1f
Sean Morrison & Eric Johnson <student@epicodus.com>
2014-07-23 12:08:55 -0700

    Refactored to replace for loop ith .each_with_index

--- lib/calc-in-words.rb
+++ lib/calc-in-words.rb
@@ -4,6 +4,7 @@ def word_calc (arg)
   num_arr = ["0","1","2","3","4","5","6","7","8","9"]
   useful_array = []
 
+
   #split input string into words,
   #filter numbers and operators into useful array
   input_array.each do |input|
@@ -20,25 +21,30 @@ def word_calc (arg)
     end
   end
 
-  for index in (0...useful_array.length)
-    if useful_array[index] == "plus"
+  # for index in (0...useful_array.length)
+  #   if useful_array[index] == "plus"
+  #     x = useful_array[index - 1]
+  #     y = useful_array[index + 1]
+  #     result = x + y
+  #   end
+  #   if useful_array[index] == "minus"
+  #     x = useful_array[index - 1]
+  #     y = useful_array[index + 1]
+  #     result = x - y
+  #   end
+  # end
+
+  useful_array.each_with_index { |item, index|
+    if (item == "plus")
       x = useful_array[index - 1]
       y = useful_array[index + 1]
-      result = x + y
+      return x + y
     end
-    if useful_array[index] == "minus"
+    if (item == "minus")
       x = useful_array[index - 1]
       y = useful_array[index + 1]
-      result = x - y
+      return x - y
     end
-  end
+  }
 
-
-
-
-
-
-
-
-  result
 end
